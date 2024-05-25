0bc33d0998a7ccf63c25d6d83f3bcb557415c1d1
Habib OULDMOUSSA <omhabibal@gmail.com>
2024-05-26 00:59:18 +0100

    Adding a Comodor 64

--- tres-vite/main.js
+++ tres-vite/main.js
@@ -1,26 +1,159 @@
+// import * as THREE from 'three';
+// import "./style.css"
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+// let camera, scene, renderer;
+
+// init();
+
+// function init() {
+
+//   const container = document.createElement('div');
+//   document.body.appendChild(container);
+
+//   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+//   camera.position.set(- 1.8, 0.6, 2.7);
+
+//   scene = new THREE.Scene();
+
+//   // model
+
+//   const loader = new GLTFLoader().setPath('./commodore_64__computer_full_pack/');
+//   loader.load('scene.gltf', async function (gltf) {
+
+//     const model = gltf.scene;
+
+//     // wait until the model can be added to the scene without blocking due to shader compilation
+
+//     await renderer.compileAsync(model, camera, scene);
+
+//     scene.add(model);
+
+//     render();
+
+//   });
+//   renderer = new THREE.WebGLRenderer({ antialias: true });
+//   renderer.setPixelRatio(window.devicePixelRatio);
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+//   renderer.toneMapping = THREE.ACESFilmicToneMapping;
+//   renderer.toneMappingExposure = 1;
+//   container.appendChild(renderer.domElement);
+
+//   const controls = new OrbitControls(camera, renderer.domElement);
+//   controls.addEventListener('change', render); // use if there is no animation loop
+//   controls.minDistance = 2;
+//   controls.maxDistance = 10;
+//   controls.target.set(0, 0, - 0.2);
+//   controls.update();
+
+//   window.addEventListener('resize', onWindowResize);
+
+// }
+
+// function onWindowResize() {
+
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+
+//   render();
+
+// }
+
+// //
+
+// function render() {
+
+//   renderer.render(scene, camera);
+
+// }
+
 import * as THREE from 'three';
+import "./style.css"
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera, scene, renderer;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+init();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function init() {
 
-camera.position.z = 5;
+  const container = document.createElement('div');
+  document.body.appendChild(container);
 
-function animate() {
-  requestAnimationFrame(animate);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+  camera.position.set(- 1.8, 0.6, 2.7);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  scene = new THREE.Scene();
+  const cube = new THREE.BoxGeometry(1, 1, 1)
+  scene.add(cube)
+  new RGBELoader()
+    .setPath('public/textures/equirectangular/')
+    .load('royal_esplanade_1k.hdr', function (texture) {
 
-  renderer.render(scene, camera);
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+
+      scene.background = texture;
+      scene.environment = texture;
+
+      render();
+
+      // model
+
+      const loader = new GLTFLoader().setPath('public/commodore_64__computer_full_pack/');
+      loader.load('scene.gltf', async function (gltf) {
+
+        const model = gltf.scene;
+
+        // wait until the model can be added to the scene without blocking due to shader compilation
+
+        await renderer.compileAsync(model, camera, scene);
+
+        scene.add(model);
+
+        render();
+
+      });
+
+    });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render); // use if there is no animation loop
+  controls.minDistance = 2;
+  controls.maxDistance = 10;
+  controls.target.set(0, 0, - 0.2);
+  controls.update();
+
+  window.addEventListener('resize', onWindowResize);
+
 }
 
-animate();
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+
+}
+
+//
+
+function render() {
+
+  renderer.render(scene, camera);
+
+}
